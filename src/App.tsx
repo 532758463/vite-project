592f56d0f436177a480eb4d00dde4bf2c4b19ff0
xiaoyang <532758463@qq.com>
2022-04-09 17:26:32 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./Button";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button></Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
